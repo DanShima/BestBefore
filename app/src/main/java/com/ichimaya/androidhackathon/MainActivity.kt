974f19e55f10e3ec5d14999e748eefd0c0185be8
Phiphi Tran <phi@live.se>
2018-10-20 13:07:42 +0200

    add bottom navigation

--- app/src/main/java/com/ichimaya/androidhackathon/MainActivity.kt
+++ app/src/main/java/com/ichimaya/androidhackathon/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ichimaya.androidhackathon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
     }
 }
