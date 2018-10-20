d4480bf767e64a88bd12fd2417a345107608995d
Meidi Tönisson <meidi@doctrin.se>
2018-10-20 14:43:27 +0200

    Add comments and convenience method

--- app/src/main/java/com/ichimaya/androidhackathon/food/model/Food.kt
+++ app/src/main/java/com/ichimaya/androidhackathon/food/model/Food.kt
@@ -2,6 +2,9 @@ package com.ichimaya.androidhackathon.food.model
 
 data class Food(val id: String,
                 val name: String,
-                val expiryDate: Long,
+                val expiryDate: Long, // expiry date in milliseconds
                 val category: String,
-                val consumeDate: Long?)
+                val consumeDate: Long?) { // consume date in milliseconds
+
+    fun isConsumed() = consumeDate != null
+}
